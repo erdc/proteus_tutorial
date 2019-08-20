0ab6a02087f69ae32342b2ca9b0f626eebeac01f
Chris Kees <cekees@gmail.com>
2019-08-20 18:30:13 -0500

    initial fixes to flood_wall

--- 2d/flood_wall.py
+++ 2d/flood_wall.py
@@ -1,17 +1,11 @@
 """
 Multiphase Flow Test
 """
-from __future__ import division
-from past.utils import old_div
 import numpy as np
-from proteus import (Domain, Context, Gauges,
-                     MeshTools as mt)
-from proteus.Gauges import PointGauges, LineIntegralGauges, LineGauges
-from proteus.Profiling import logEvent
+from proteus import (Domain, Context)
 import proteus.TwoPhaseFlow.TwoPhaseFlowProblem as TpFlow
 import math
 from proteus.ctransportCoefficients import smoothedHeaviside
-from proteus.ctransportCoefficients import smoothedHeaviside_integral
 from proteus import WaveTools as wt
 from proteus.mprans import SpatialTools as st
 
@@ -24,14 +18,13 @@ opts= Context.Options([
     ("dt_output",0.1,"Time interval to output solution"),
     ("gauges", True, "Collect data for validation"),
     ("cfl",0.2,"Desired CFL restriction"),
-    ("he",0.5,"Max mesh element diameter"),
-    ("ARTIFICIAL_VISCOSITY",3,"artificial viscosity")
+    ("he",0.08,"Max mesh element diameter")
     ])
 
 waterLevel=0.75
 leeward_wl=0.25
 pro_wl=0.5
-g=np.array([0.,0.0,-9.81])
+g=np.array([0.,-9.81,0.0])
 he=opts.he
 # ****************** #
 # ***** GAUGES ***** #
@@ -42,17 +35,15 @@ he=opts.he
 # *************************** #
 domain = Domain.PlanarStraightLineGraphDomain()
 
-boundaries=['gate','left','right','bottom','top','front','back']
+boundaries=['gate','left','right','bottom','top']
 boundaryTags=dict([(key,i+1) for (i,key) in enumerate(boundaries)])
 
-boundaryOrientations = {'gate': np.array([-1., 0.,0.]),
+boundaryOrientations = {'gate': np.array([0, 1.,0.]),
                         'left': np.array([-1., 0.,0.]),
                         'right': np.array([+1., 0.,0.]),
-                        'bottom': np.array([0., 0.,-1.]),
-                        'top': np.array([0., 0.,+1.]),
-                        'front': np.array([0.,+1.,0.]),
-                        'back': np.array([0.,-1., 0.]),
-                            }
+                        'top': np.array([0.,+1.,0.]),
+                        'bottom': np.array([0.,-1., 0.]),
+}
 vertices=[[0.0,0.0],#0
           [2.0,0.0],#1
           [2.1,1.0],#2
@@ -61,43 +52,40 @@ vertices=[[0.0,0.0],#0
           [3.5,0.0],#5
           [3.5,1.5],#6
           [0.0,1.5],#7
-          ]
+]
 
 vertexFlags=[boundaryTags['bottom'],
-             boundaryTags['bottom'],
-             boundaryTags['bottom'],
-             boundaryTags['bottom'],
-             boundaryTags['bottom'],
+             boundaryTags['gate'],
+             boundaryTags['gate'],
+             boundaryTags['gate'],
+             boundaryTags['gate'],
              boundaryTags['bottom'],
              boundaryTags['top'],
              boundaryTags['top'],
              ]
 
 segments=[[0,1],
-        [1,2],
-        [2,3],
-        [3,4],
-        [4,5],
-        [5,6],
-        [6,7],
-        [7,0]]
+          [1,2],
+          [2,3],
+          [3,4],
+          [4,5],
+          [5,6],
+          [6,7],
+          [7,0]]
 
 
 
 segmentFlags=[boundaryTags['bottom'],
-            boundaryTags['bottom'],
-            boundaryTags['bottom'],
-            boundaryTags['bottom'],
-#            boundaryTags['bottom'],
-#            boundaryTags['bottom'],
-            boundaryTags['right'],
-            boundaryTags['top'],
-            boundaryTags['top'],
-            boundaryTags['left']]
-        
-regions=[[0.1,0.1,0.1]]
-regionFlags=[1]
+              boundaryTags['gate'],
+              boundaryTags['gate'],
+              boundaryTags['gate'],
+              boundaryTags['bottom'],
+              boundaryTags['right'],
+              boundaryTags['top'],
+              boundaryTags['left']]
 
+regions=[[0.1,0.1]]
+regionFlags=[1]
 
 tank = st.CustomShape(domain,
                       vertices=vertices,
@@ -109,30 +97,28 @@ tank = st.CustomShape(domain,
                       boundaryTags=boundaryTags,
                       boundaryOrientations=boundaryOrientations)
 
-tank.BC['top'].setAtmosphere()
-#tank.BC['top'].setFreeSlip()
+# ******************************* #
+# ***** BOUNDARY CONDITIONS ***** #
+# ******************************* #
+
+wave=wt.MonochromaticWaves(period=1.0,
+                           waveHeight=0.25,
+                           mwl=waterLevel,
+                           depth=waterLevel,
+                           g=g,
+                           waveDir=np.array([1.,0.,0.]))
+tank.BC['left'].setUnsteadyTwoPhaseVelocityInlet(wave, smoothing=1.5*he)
 tank.BC['bottom'].setFreeSlip()
-#tank.BC['gate'].setFreeSlip()
-#tank.BC['front'].setFreeSlip()
-#tank.BC['back'].setFreeSlip()
-#tank.BC['left'].setFreeSlip()
+tank.BC['gate'].setFreeSlip()
 tank.BC['right'].setFreeSlip()
-
-
-wave=wt.MonochromaticWaves(period=1.0,waveHeight=0.5,mwl=waterLevel,depth=waterLevel,g=g,waveDir=np.array([1.0,0.,0.]))
-tank.BC['left'].setUnsteadyTwoPhaseVelocityInlet(wave, smoothing=3.*he)
+tank.BC['top'].setAtmosphere()
 
 domain.MeshOptions.setParallelPartitioningType('node')
 domain.boundaryTags = boundaryTags
-he = opts.he
-domain.MeshOptions.he = he
+domain.MeshOptions.he = opts.he
 st.assembleDomain(domain)
-#domain.MeshOptions.triangleOptions="VApq1.25q12feena%e" % ((he**2)/2.0,)
-triangleOptions = "VApq30Dena%8.8f" % ((he**2)/2.0,)   
-domain.writePLY("mesh")
-#domain.readPoly("SG")
+triangleOptions = "VApq30Dena%8.8f" % ((opts.he**2)/2.0,)   
 domain.writePoly("mesh")
-domain.writeAsymptote("mesh")
 
 # ****************************** #
 # ***** INITIAL CONDITIONS ***** #
@@ -142,36 +128,26 @@ class zero(object):
         return 0.
 
 def signedDistance(x):
- #   if x[0]>2.1:
- #       return x[2] - leeward_wl
- #   else:
-    return x[2]-waterLevel
+    phi_x = x[0] - 2.15
+    phi_y = x[1] - waterLevel
+    if phi_x < 0.0:
+        if phi_y < 0.0:
+            return max(phi_x, phi_y)
+        else:
+            return phi_y
+    else:
+        if phi_y < 0.0:
+            return phi_x
+        else:
+            return (phi_x ** 2 + phi_y ** 2)**0.5
 
-    # a = ((x[0]-19)**2+(x[1]-4.5)**2)**0.5
-    # b = ((x[0]-19)**2+(x[1]+4.5)**2)**0.5
-
-    # if x[0]>18.0:
-    #     return x[2] - pro_wl
-    # elif a<4 or b<4:
-    #     return x[2] - pro_wl
-    # else:
-    #     return x[2] - waterLevel
-
-epsFact_consrv_heaviside=3.0
 class VF_IC:
     def uOfXT(self, x, t):
-        return smoothedHeaviside(epsFact_consrv_heaviside*he,signedDistance(x))
+        return smoothedHeaviside(1.5*opts.he,signedDistance(x))
 
 class PHI_IC:
     def uOfXT(self, x, t):
         return signedDistance(x)
-
-
-# ******************************* #
-# ***** BOUNDARY CONDITIONS ***** #
-# ******************************* #
-    
-# ADVECTIVE FLUX BOUNDARY CONDITIONS #
     
 ############################################
 # ***** Create myTwoPhaseFlowProblem ***** #
@@ -191,15 +167,6 @@ myTpFlowProblem = TpFlow.TwoPhaseFlowProblem(ns_model=0,
                                              nd=2,
                                              cfl=opts.cfl,
                                              outputStepping=outputStepping,
-                                             structured=False,
                                              he=he,
-                                             nnx=None,
-                                             nny=None,
-                                             nnz=None,
                                              domain=domain,
-                                             initialConditions=initialConditions,
-                                             boundaryConditions=None,
-                                             auxVariables=None,
-                                             useSuperlu=False)
-#myTpFlowProblem.physical_parameters['gravity'] = [0.0,0.0,-9.81]
-myTpFlowProblem.Parameters.physical.gravity = [0., -9.81,0.0]
+                                             initialConditions=initialConditions)
